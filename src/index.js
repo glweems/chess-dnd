9999864dd409114637dfac8aebac721a4a8d1c73
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-29 11:14:44 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Example from './Example'
+import Example from './example'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 
